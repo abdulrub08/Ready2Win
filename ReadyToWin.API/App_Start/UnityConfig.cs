e5f81cf8eb3b3f3ed3848438642aac1f7b4e2bcb
Abdul <abdulrub44469@gmail.com>
2022-09-07 12:41:27 +0530

    Setup Project1

---
+++ ReadyToWin.API/App_Start/UnityConfig.cs
@@ -0,0 +1,32 @@
+using ReadyToWin.API.Filters;
+using ReadyToWin.API.Models;
+using ReadyToWin.Complaince.Bussiness.Provider.UserRepositry;
+using ReadyToWin.Complaince.DataAccess.Repository;
+using ReadyToWin.Complaince.DataAccess.UserRepositry;
+using ReadyToWin.Complaince.BussinessProvider.IProviders;
+using System.Web.Http;
+using System.Web.Mvc;
+using Unity;
+using Unity.WebApi;
+
+namespace ReadyToWin.API
+{
+    public static class UnityConfig
+    {
+        public static void RegisterComponents(HttpConfiguration config)
+        {
+			var container = new UnityContainer();
+            
+            // register all your components with the container here
+            // it is NOT necessary to register your controllers
+            
+            // e.g. container.RegisterType<ITestService, TestService>();
+
+            container.RegisterType<IGameType, GameTypeRepository>();
+            container.RegisterType<IUserRepository, UserRepository>();
+            container.RegisterType<IUserTransaction, UserTransaction>();
+            //GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
+            config.DependencyResolver = new UnityResolver(container);
+        }
+    }
+}
---
+++ ReadyToWin.API/Startup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using Microsoft.Owin.Cors;
+
+[assembly: OwinStartup(typeof(ReadyToWin.API.Startup))]
+
+namespace ReadyToWin.API
+{
+    public class Startup
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="app"></param>
+        public void Configuration(IAppBuilder app)
+        {
+            //ConfigureAuth(app);
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            UnityConfig.RegisterComponents(config);
+            app.UseWebApi(config);
+            ////enable cors 
+            app.UseCors(CorsOptions.AllowAll);
+        }  
+    }
+}
